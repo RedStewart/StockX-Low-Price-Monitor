44ddb13b4fafff61cbd32e49a8c2e1ebb3ca1942
Reed Stewart <reedostewart@gmail.com>
2018-08-23 19:02:03 +1200

    Added tick function for the moitor, takes specified interval time from config and runs code every time, testing to be done, only currently implemented 1 url, added webhook for discord package

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ function checkTime(i) {
 }
 
 function requestURL(url, index) {
-    console.log(time + " Sending request...");
     request("https://stockx.com/" + url, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var price, size;
@@ -33,23 +32,23 @@ function requestURL(url, index) {
             //prices
             $('div.mobile-header-inner > div.options > div.form-group > div.select-control > div.select-options > ul.list-unstyled > li.select-option > div.inset > div.subtitle').each((i, el) => {
                 price = $(el).text();
-                if(price.startsWith("$")){
+                if (price.startsWith("$")) {
                     priceTest = price.substr(1);
                     priceArr.push(parseInt(priceTest));
                 }
                 else
-                    priceArr.push(price);
+                    priceArr.push(0);
             });
             //size
             $('div.mobile-header-inner > div.options > div.form-group > div.select-control > div.select-options > ul.list-unstyled > li.select-option > div.inset > div.title').each((i, el) => {
                 size = $(el).text();
                 sizeArr.push(size);
             });
-            
+
             shoeArr.push(sizeArr, priceArr);
-            
+
             //add shoe data to respective array from index given (temporary until better fix)
-            if(index == 1)
+            if (index == 1)
                 initArr.push(shoeArr);
             else
                 compareArr.push(shoeArr);
@@ -57,41 +56,56 @@ function requestURL(url, index) {
         else {
             console.log("[ERROR] " + error);
         }
-    })    
+    })
 }
 
-//main
-function main() {
-    for (x = 0; x < config.shoeURL.length; x++) {
-        urlArr.push(config.shoeURL[x]);
-    }
-    //populate initial array full of desired shoes
-    for (x = 0; x < urlArr.length; x++) {
-        requestURL(urlArr[x], 1);
-    }
-    
+function compareArrays() {
+    console.log(time + ' Creating comparison array');
     //have to make timeout as request is async (temporary until better fix)
     setTimeout(function () {
         for (x = 0; x < urlArr.length; x++) {
             requestURL(urlArr[x], 0);
         }
-        
+
         setTimeout(function () {
-            for(x = 0; x < initArr[0].length - 1; x++){
-                for(y = 0; y < initArr[x][1].length; y++){
+            console.log(time + ' Comparing prices');
+            for (x = 0; x < initArr[0].length - 1; x++) {
+                for (y = 0; y < initArr[x][1].length; y++) {
                     var initPrice = initArr[x][1][y];
                     var comparePrice = compareArr[x][1][y];
 
-                    if(initPrice == comparePrice)
-                        console.log('yeet');
-
+                    if (initPrice < comparePrice) {
+                        console.log('less yeet');
+                    }
+                    else if (initPrice > comparePrice) {
+                        console.log('more yeet');
+                    }
+                    else {
+                        console.log('No change found');
+                    }
                 }
             }
-
         }, 3000);
-        
     }, 3000);
 }
 
+//main
+function main() {
+    console.log(time + ' Starting...');
+    var interval = config.interval;
+
+    console.log(time + ' Finding requested shoes');
+    for (x = 0; x < config.shoeURL.length; x++) {
+        urlArr.push(config.shoeURL[x]);
+    }
+
+    console.log(time + ' Gathering initial prices');
+    //populate initial array full of desired shoes
+    for (x = 0; x < urlArr.length; x++) {
+        requestURL(urlArr[x], 1);
+    }
+    //tick
+    intervalTimer = setInterval(compareArrays, interval);
+}
 
 main();
