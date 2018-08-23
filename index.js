c6c25b011ee637bb404063a6d3cfb1164538d948
Reed Stewart <reedostewart@gmail.com>
2018-08-23 22:25:33 +1200

    Implemented multi url support, still buggy, still using delay which would rather not, webhook not working as getting 404 error, fixed timer to display actual time

--- index.js
+++ index.js
@@ -1,111 +1,131 @@
-const request = require('request');
-const cheerio = require('cheerio');
-const path = require('path');
+try {
+    const request = require('request');
+    const cheerio = require('cheerio');
+    const path = require('path');
+    const Webhook = require('webhook-discord');
 
-const urlArr = [];
-const initArr = [];
-const compareArr = [];
-const config = require(path.join(__dirname, 'config.json'));
+    const urlArr = [];
+    const initArr = [];
+    var compareArr = [];
+    const config = require(path.join(__dirname, 'config.json'));
+    const Hook = new Webhook(config.webhook.webhookUrl);
 
-var today = new Date(),
-    h = checkTime(today.getHours()),
-    m = checkTime(today.getMinutes()),
-    s = checkTime(today.getSeconds());
-ms = checkTime(today.getMilliseconds());
-var time = "[" + h + ":" + m + ":" + s + ":" + ms + "]";
+    function getTime() {
+        var today = new Date();
+        h = checkTime(today.getHours());
+        m = checkTime(today.getMinutes());
+        s = checkTime(today.getSeconds());
+        ms = checkTime(today.getMilliseconds());
+        return time = "[" + h + ":" + m + ":" + s + ":" + ms + "]";
+    }
+    function checkTime(i) {
+        return (i < 10) ? "0" + i : i;
+    }
 
-function checkTime(i) {
-    return (i < 10) ? "0" + i : i;
-}
+    function requestURL(url, index) {
+        try {
+            setTimeout(function () {
+                request("https://stockx.com/" + url, function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        var price, size;
+                        const shoeArr = [];
+                        const priceArr = [];
+                        const sizeArr = [];
 
-function requestURL(url, index) {
-    request("https://stockx.com/" + url, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var price, size;
-            const shoeArr = [];
-            const priceArr = [];
-            const sizeArr = [];
+                        var $ = cheerio.load(body);
+                        console.log(getTime() + " Scraping " + $('h1').text());
 
-            var $ = cheerio.load(body);
-            console.log("Scraping " + $('h1').text());
+                        //prices
+                        $('div.mobile-header-inner > div.options > div.form-group > div.select-control > div.select-options > ul.list-unstyled > li.select-option > div.inset > div.subtitle').each((i, el) => {
+                            price = $(el).text();
+                            if (price.startsWith("$")) {
+                                priceTest = price.substr(1);
+                                priceArr.push(parseInt(priceTest));
+                            }
+                            else
+                                priceArr.push(0);
+                        });
+                        //size
+                        $('div.mobile-header-inner > div.options > div.form-group > div.select-control > div.select-options > ul.list-unstyled > li.select-option > div.inset > div.title').each((i, el) => {
+                            size = $(el).text();
+                            sizeArr.push(size);
+                        });
 
-            //prices
-            $('div.mobile-header-inner > div.options > div.form-group > div.select-control > div.select-options > ul.list-unstyled > li.select-option > div.inset > div.subtitle').each((i, el) => {
-                price = $(el).text();
-                if (price.startsWith("$")) {
-                    priceTest = price.substr(1);
-                    priceArr.push(parseInt(priceTest));
-                }
-                else
-                    priceArr.push(0);
-            });
-            //size
-            $('div.mobile-header-inner > div.options > div.form-group > div.select-control > div.select-options > ul.list-unstyled > li.select-option > div.inset > div.title').each((i, el) => {
-                size = $(el).text();
-                sizeArr.push(size);
-            });
+                        shoeArr.push(sizeArr, priceArr);
 
-            shoeArr.push(sizeArr, priceArr);
-
-            //add shoe data to respective array from index given (temporary until better fix)
-            if (index == 1)
-                initArr.push(shoeArr);
-            else
-                compareArr.push(shoeArr);
-        }
-        else {
-            console.log("[ERROR] " + error);
-        }
-    })
-}
-
-function compareArrays() {
-    console.log(time + ' Creating comparison array');
-    //have to make timeout as request is async (temporary until better fix)
-    setTimeout(function () {
-        for (x = 0; x < urlArr.length; x++) {
-            requestURL(urlArr[x], 0);
-        }
-
-        setTimeout(function () {
-            console.log(time + ' Comparing prices');
-            for (x = 0; x < initArr[0].length - 1; x++) {
-                for (y = 0; y < initArr[x][1].length; y++) {
-                    var initPrice = initArr[x][1][y];
-                    var comparePrice = compareArr[x][1][y];
-
-                    if (initPrice < comparePrice) {
-                        console.log('less yeet');
-                    }
-                    else if (initPrice > comparePrice) {
-                        console.log('more yeet');
+                        //add shoe data to respective array from index given (temporary until better fix)
+                        if (index == 1)
+                            initArr.push(shoeArr);
+                        else
+                            compareArr.push(shoeArr);
                     }
                     else {
-                        console.log('No change found');
+                        console.log("[ERROR] " + error);
+                    }
+                })
+            }, 1000);
+        } catch (err) {
+            console.log('[ERROR] ' + err);
+        }
+    }
+
+    function compareArrays() {
+        initArr.sort();
+
+        console.log(getTime() + ' Creating comparison array');
+        //have to make timeout as request is async (temporary until better fix)
+        setTimeout(function () {
+            for (x = 0; x < urlArr.length; x++) {
+                requestURL(urlArr[x], 0);
+            }
+
+            setTimeout(function () {
+                compareArr.sort();
+                console.log(compareArr);
+                console.log(getTime() + ' Comparing prices');
+                for (x = 0; x < compareArr[0].length; x++) {
+                    for (y = 0; y < initArr[x][1].length; y++) {
+                        var initPrice = initArr[x][1][y];
+                        var comparePrice = compareArr[x][1][y];
+
+                        if (initPrice < comparePrice) {
+                            console.log('less yeet');
+                        }
+                        else if (initPrice > comparePrice) {
+                            console.log('more yeet');
+                        }
+                        else {
+                            console.log('No change found');
+                        }
                     }
                 }
-            }
-        }, 3000);
-    }, 3000);
-}
-
-//main
-function main() {
-    console.log(time + ' Starting...');
-    var interval = config.interval;
-
-    console.log(time + ' Finding requested shoes');
-    for (x = 0; x < config.shoeURL.length; x++) {
-        urlArr.push(config.shoeURL[x]);
+            }, 10000);
+        }, 5000);
+        compareArr = [];
     }
 
-    console.log(time + ' Gathering initial prices');
-    //populate initial array full of desired shoes
-    for (x = 0; x < urlArr.length; x++) {
-        requestURL(urlArr[x], 1);
-    }
-    //tick
-    intervalTimer = setInterval(compareArrays, interval);
-}
+    //main
+    function main() {
+        console.log(getTime() + ' Starting...');
+        var interval = config.interval;
 
-main();
+        console.log(getTime() + ' Finding requested shoes');
+        for (x = 0; x < config.shoeURL.length; x++) {
+            urlArr.push(config.shoeURL[x]);
+        }
+
+        console.log(getTime() + ' Gathering initial prices');
+        //populate initial array full of desired shoes
+        for (x = 0; x < urlArr.length; x++) {
+            requestURL(urlArr[x], 1);
+        }
+
+        //tick
+        intervalTimer = setInterval(compareArrays, interval);
+
+    }
+
+    main();
+} catch (err) {
+    console.log('[ERROR] ' + err);
+}
