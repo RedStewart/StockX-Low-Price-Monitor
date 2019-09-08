46773ce2eb199b4442c6d321af70ff8919f619a3
Reed Stewart <reedostewart@gmail.com>
2019-09-08 20:19:26 +1200

    Constructing the shoe object for when StockX is requested

--- index.js
+++ index.js
@@ -1,62 +1,70 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-const path = require('path');
-const Webhook = require('webhook-discord');
-const shoe = require('./Shoe');
+const axios = require("axios");
+const cheerio = require("cheerio");
+const path = require("path");
+const Webhook = require("webhook-discord");
+const shoe = require("./Shoe");
 const Shoe = shoe.Shoe;
 const colourArr = [
-  '#FF3855',
-  '#FF7A00',
-  '#FDFF00',
-  '#87FF2A',
-  '#4F86F7',
-  '#DB91EF',
-  '#6F2DA8'
+  "#FF3855",
+  "#FF7A00",
+  "#FDFF00",
+  "#87FF2A",
+  "#4F86F7",
+  "#DB91EF",
+  "#6F2DA8"
 ];
-const config = require(path.join(__dirname, 'config.json'));
+const config = require(path.join(__dirname, "config.json"));
 let shoeUrl = config.shoeURL;
 
 const requestURL = async () => {
-  let shoeArr = [];
-  //   for (let x = 0; x < shoeUrl.length; x++) {
-  //let beforeTime = Date.now();
+  // let shoes = [];
+  let shoeObj = {
+    shoes: []
+  };
+  for (let x = 0; x < shoeUrl.length; x++) {
+    try {
+      let url = `https://stockx.com/${shoeUrl[x]}`;
+      let response = await axios.get(url, {
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36",
+          Accept:
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+          "Accept-Encoding": "gzip, deflate, br",
+          "sec-fetch-user": "?1"
+        }
+      });
+      let $ = cheerio.load(response.data);
 
-  try {
-    let url = `https://stockx.com/${shoeUrl[0]}`;
-    let response = await axios.get(url, {
-      headers: {
-        'User-Agent':
-          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36',
-        Accept:
-          'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'sec-fetch-user': '?1'
+      let shoeName = $("h1").text();
+      let sizeArr = getSizes($);
+      let priceArr = getPrices($);
+      for(let i = 0; i < sizeArr; i++) {
+        shoeObj.shoes[x]= {sizePrice: {size: sizeArr[i], price: priceArr[i]}}
       }
-    });
-    let $ = cheerio.load(response.data);
+      let shoeImage = $("div.image-container > img").attr("src");
 
-    let shoeName = $('h1').text();
-    let sizeArr = getSizes($);
-    let priceArr = getPrices($);
-    let shoeImage = $('div.image-container > img').attr('src');
+      shoeObj.shoes[x] = {
+        name: shoeName,
+        url,
+        imgSrc: shoeImage
+      };
 
-    shoeArr.push(new Shoe(shoeName, sizeArr, priceArr, url, shoeImage));
+      // shoeArr.push(new Shoe(shoeName, sizeArr, priceArr, url, shoeImage));
 
-    console.log(shoeName, sizeArr, priceArr, shoeImage);
-  } catch (e) {
-    console.log(e);
+    } catch (e) {
+      // do something here for if the request fails
+      console.log(e);
+    }
   }
-
-  //console.log("Time elapsed: [" + (Date.now() - beforeTime) / 1000 + "] " + shoeName);
-
-  return shoeArr;
+  return shoeObj;
 };
 
 async function comparePrices(initialArr) {
   const Hook = new Webhook.Webhook(config.webhook);
 
   let compareArr = await requestURL();
-  console.log(logTime() + 'Comparing prices...\n');
+  console.log(logTime() + "Comparing prices...\n");
 
   // for (var x = 0; x < 2; x++) {
   for (var x = 0; x < initialArr.length; x++) {
@@ -77,30 +85,30 @@ async function comparePrices(initialArr) {
           if (priceDiffRound > 5) {
             console.log(
               logTime() +
-                'SENDING WEBHOOK\n=============================\n' +
+                "SENDING WEBHOOK\n=============================\n" +
                 compareArr[x].name +
-                '\nSize: ' +
+                "\nSize: " +
                 compareArr[x].sizes[i] +
-                '\nNew Low Price: $' +
+                "\nNew Low Price: $" +
                 comparePrice +
-                '.00' +
-                '\nOld Price: $' +
+                ".00" +
+                "\nOld Price: $" +
                 initialPrice +
-                '.00' +
-                '\n' +
+                ".00" +
+                "\n" +
                 priceDiffRound +
-                '% decrease in price\n'
+                "% decrease in price\n"
             );
 
             const hookMsg = new Webhook.MessageBuilder()
-              .setName('StockX Low Price Monitor')
+              .setName("StockX Low Price Monitor")
               .setColor(colourArr[Math.floor(Math.random() * colourArr.length)])
               .setTitle(compareArr[x].name)
-              .addField('Size: ' + compareArr[x].sizes[i], '')
-              .addField('New Low Price: $' + comparePrice + '.00', '')
-              .addField('', 'Old Price: $' + initialPrice + '.00')
-              .addField('', priceDiffRound + '% decrease in price')
-              .addField('Link: ' + compareArr[x].url)
+              .addField("Size: " + compareArr[x].sizes[i], "")
+              .addField("New Low Price: $" + comparePrice + ".00", "")
+              .addField("", "Old Price: $" + initialPrice + ".00")
+              .addField("", priceDiffRound + "% decrease in price")
+              .addField("Link: " + compareArr[x].url)
               .setImage(compareArr[x].image);
 
             Hook.send(hookMsg);
@@ -120,7 +128,7 @@ async function comparePrices(initialArr) {
 const getSizes = $ => {
   let sizeArr = [];
   $(
-    'div.mobile-header-inner > div.options > div.form-group > div.select-control > div.select-options > ul.list-unstyled > li.select-option > div.inset > div.title'
+    "div.mobile-header-inner > div.options > div.form-group > div.select-control > div.select-options > ul.list-unstyled > li.select-option > div.inset > div.title"
   ).each((i, el) => {
     let size = $(el).text();
     sizeArr.push(size);
@@ -132,32 +140,33 @@ const getSizes = $ => {
 const getPrices = $ => {
   let priceArr = [];
   $(
-    'div.mobile-header-inner > div.options > div.form-group > div.select-control > div.select-options > ul.list-unstyled > li.select-option > div.inset > div.subtitle'
+    "div.mobile-header-inner > div.options > div.form-group > div.select-control > div.select-options > ul.list-unstyled > li.select-option > div.inset > div.subtitle"
   ).each((i, el) => {
     //price of shoe
     let price = $(el).text();
     //check if it isnt 0
-    if (price.startsWith('$')) {
+    if (price.startsWith("$")) {
       //strip the dollar sign
       priceFilter = price.substr(1);
       //make int so we can compare
-      priceArr.push(parseInt(priceFilter.replace(',', '')));
+      priceArr.push(parseInt(priceFilter.replace(",", "")));
     } else priceArr.push(0);
   });
   return priceArr;
 };
 
 const main = async () => {
-  if (config.webhook === '' || undefined) {
+  if (config.webhook === "" || undefined) {
     console.error(
-      'ERROR: Please enter a valid webhook URL in the config file\n'
+      "ERROR: Please enter a valid webhook URL in the config file\n"
     );
   } else {
-    console.log(logTime() + 'Monitor starting...');
-    console.log(logTime() + 'Scraping for initial prices');
+    console.log(logTime() + "Monitor starting...");
+    console.log(logTime() + "Scraping for initial prices");
 
-    var initialArr = await requestURL();
-    console.log(logTime() + 'Initial prices gathered\n');
+    let initialArr = await requestURL();
+    console.log(logTime() + "Initial prices gathered\n");
+    console.log(initialArr);
 
     //   setInterval(async () => {
     //     await comparePrices(initialArr);
@@ -173,12 +182,12 @@ function logTime() {
   let m = formatTime(date.getMinutes(), 2);
   let s = formatTime(date.getSeconds(), 2);
   let ms = formatTime(date.getMilliseconds(), 3);
-  return (date = '[' + h + ':' + m + ':' + s + ':' + ms + '] ');
+  return (date = "[" + h + ":" + m + ":" + s + ":" + ms + "] ");
 }
 
 function formatTime(x, n) {
   while (x.toString().length < n) {
-    x = '0' + x;
+    x = "0" + x;
   }
   return x;
 }
